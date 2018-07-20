35acbc5f629aa7b9aa1e90cd40254b057bb803c9
Jonathan <sellsj14@gmail.com>
2018-07-20 08:53:24 -0400

    linked the css reset and the js file, working

---
+++ assets/javascript/game.js
@@ -0,0 +1,5 @@
+//dont forget to wrap everthing in a ready function so your code does not run before the DOM is constructed 
+$(document).ready(function(){
+    document.getElementById("test").innerHTML = "hello world, I love js"
+});
+    
