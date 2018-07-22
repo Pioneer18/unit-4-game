68ac32d888da95b997b35f3020ec659a36bd4da9
Jonathan <sellsj14@gmail.com>
2018-07-22 12:36:38 -0400

    there is now a linked js file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,36 @@
 //dont forget to wrap everthing in a ready function so your code does not run before the DOM is constructed 
-$(document).ready(function(){
-    document.getElementById("test").innerHTML = "hello world, I love js"
+/*
+    Here's how the app works:
+
+   * There will be four crystals displayed as buttons on the page.
+
+   * The player will be shown a random number at the start of the game.
+
+   * When the player clicks on a crystal, it will add a specific amount of points to the player's total score. 
+
+     * Your game will hide this amount until the player clicks a crystal.
+     * When they do click one, update the player's score counter.
+
+   * The player wins if their total score matches the random number from the beginning of the game.
+
+   * The player loses if their score goes above the random number.
+
+   * The game restarts whenever the player wins or loses.
+
+     * When the game begins again, the player should see a new random number. Also, all the crystals will have four new hidden values. Of course, the user's score (and score counter) will reset to zero.
+
+   * The app should show the number of games the player wins and loses. To that end, do not refresh the page as a means to restart the game.
+
+    ##### Option 1 Game design notes
+
+    * The random number shown at the start of the game should be between 19 - 120.
+
+    * Each crystal should have a random hidden value between 1 - 12.
+
+*/
+//dont forget to wrap your js in the ready function so the js wont attempt anything before the DOM is fully loaded
+document.ready(function(){
+    //create four responsive crystals (buttons)
+   
 });
     
