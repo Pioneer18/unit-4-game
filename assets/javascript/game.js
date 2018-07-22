b26457e30f032c1ec5f4bd83b9bb078fc6d1a8d3
Jonathan <sellsj14@gmail.com>
2018-07-22 18:14:10 -0400

    the game logic is working; can streamline program with funcitons, need to polish the front end

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,8 @@
 //dont forget to wrap your js in the ready function so the js wont attempt anything before the DOM is fully loaded
 $(document).ready(function(){
     //initialize the global variables
+    var wins = 0;
+    var losses = 0;
     //this the value holder for the player total score
     var crystal_count = 0;
     document.getElementById("crystal_count").innerHTML = crystal_count;
@@ -59,16 +61,30 @@ $(document).ready(function(){
 
       //The player will gain a win if the crystal_count equals the crystal_target and they will lose if it goes over
       //On either a win or loss the game will reset the crystal_target, the button values, and the crystal-count will be 0
+      //TODO: make this into a funciton
       if(crystal_count >= crystal_target){
-        //reset the target and the buttons values
-        crystal_target = Math.floor(Math.random()*102)+19;
-        document.getElementById("crystal_target").innerHTML = crystal_target;
-        crystal_count = 0;
-        document.getElementById("crystal_count").innerHTML = crystal_count;
+        //reset the buttons values
         crystal_1 = Math.floor(Math.random()*11)+1;  
         crystal_2 = Math.floor(Math.random()*11)+1;
         crystal_3 = Math.floor(Math.random()*11)+1;
         crystal_4 = Math.floor(Math.random()*11)+1;
+        console.log("the new secret crystal values " + crystal_1, crystal_2, crystal_3,crystal_4);
+        if(crystal_count == crystal_target){
+          wins++;
+          console.log("wins " + wins);
+          crystal_target = Math.floor(Math.random()*102)+19;
+          document.getElementById("crystal_target").innerHTML = crystal_target;
+          crystal_count = 0;
+          document.getElementById("crystal_count").innerHTML = crystal_count;
+        }
+        else if(crystal_count > crystal_target){
+          losses++;
+          console.log("losses " + losses);
+          crystal_target = Math.floor(Math.random()*102)+19;
+          document.getElementById("crystal_target").innerHTML = crystal_target;
+          crystal_count = 0;
+          document.getElementById("crystal_count").innerHTML = crystal_count;
+        }
       }
     });
 
