b6e90fc6709f1d4f1e2704785453c6d823321c13
Jonathan <sellsj14@gmail.com>
2018-07-22 23:40:26 -0400

    optimized use of functions, corrected comments

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,78 +7,22 @@ $(document).ready(function(){
     //initialize the global variables
     var wins = 0;
     var losses = 0;
-    //this the value holder for the player total score
+    //this is the value holder for the player total score
     var crystal_count = 0;
     $("#crystal_count").html(crystal_count);
     //this is the randomly generated target number 19-120
     var crystal_target = new_target();
     $("#crystal_target").html(crystal_target);
-    //this statement should randomly generate a number for each crystal
+    //below is assigning each crystal a random secret value between 1 and 12
     var crystal_1 = new_crystal(), crystal_2 = new_crystal(),
     crystal_3 = new_crystal(),crystal_4 = new_crystal();
    
-    //This section starts the onclick function that will control the rest of the game
-    //there will be one onclick that targets the class of button
-    //then the id of the button is found and the crystal count is updated by that button's
-    //secret_value
+    //This JQuery button selector will run the game logic when the user chooses a crystal
     $(".button").click(function(){
-      //the rest of the game must be contained within this onclick funciton
-      //ID which button was clicked
-      
+      //ID which crystal (button) was clicked
       var userPick = this.id
-      //TODO: make this if block a function called crystal_selection()
-      if(userPick == "cr_1"){
-        //update the crystal_count with the value of the clicked button
-        crystal_count += crystal_1;
-        $("#crystal_count").html(crystal_count);
-      }
-      
-      if(userPick == "cr_2"){
-        //update the crystal_count with the value of the clicked button
-        crystal_count += crystal_2;
-        //udpate the crystal_count display
-        $("#crystal_count").html(crystal_count);
-      }
-      if(userPick == "cr_3"){
-        //update the crystal_count with the value of the clicked button
-        crystal_count += crystal_3;
-        //udpate the crystal_count display
-        $("#crystal_count").html(crystal_count);
-      }
-      if(userPick == "cr_4"){
-        //update the crystal_count with the value of the clicked button
-        crystal_count += crystal_4;
-        //udpate the crystal_count display
-        $("#crystal_count").html(crystal_count);
-      }
-      
-
-      //The player will gain a win if the crystal_count equals the crystal_target and they will lose if it goes over
-      //On either a win or loss the game will reset the crystal_target, the button values, and the crystal-count will be 0
-      //TODO: make this into a funciton
-      if(crystal_count >= crystal_target){
-        //reset the buttons values
-        crystal_1 = new_crystal();  
-        crystal_2 = new_crystal();
-        crystal_3 = new_crystal();
-        crystal_4 = new_crystal();
-        if(crystal_count == crystal_target){
-          wins++;
-          $("#wins").html("Wins: " + wins);
-          crystal_target = new_target();
-          $("#crystal_target").html(crystal_target);
-          crystal_count = 0;
-          $("#crystal_count").html(crystal_count);
-        }
-        else if(crystal_count > crystal_target){
-          losses++;
-          $("#losses").html("Losses: " + losses);
-          crystal_target = new_target();
-          $("#crystal_target").html(crystal_target);
-          crystal_count = 0;
-          $("#crystal_count").html(crystal_count);
-        }
-      }
+      //run the game logic
+      crystal_collector(userPick);
     });
 
     //functions to be called
@@ -89,9 +33,67 @@ $(document).ready(function(){
       return secret_value;
     }
 
+    //generates a new crystal_target
     function new_target(){
       var new_value = Math.floor(Math.random()*102)+19;
       return new_value;
+    }
+
+    //handles the logic for the game
+    function crystal_collector(userPick){
+      if(userPick == "cr_1"){
+        //update the crystal_count with crystal 1 secret value
+        crystal_count += crystal_1;
+        $("#crystal_count").html(crystal_count);
+      }
+      
+      if(userPick == "cr_2"){
+        //update the crystal_count with crystal 2 secret value
+        crystal_count += crystal_2;
+        //udpate the crystal_count display
+        $("#crystal_count").html(crystal_count);
+      }
+      if(userPick == "cr_3"){
+        //update the crystal_count with crystal 3 secret value
+        crystal_count += crystal_3;
+        //udpate the crystal_count display
+        $("#crystal_count").html(crystal_count);
+      }
+      if(userPick == "cr_4"){
+        //update the crystal_count with crystal 4 secret value
+        crystal_count += crystal_4;
+        //udpate the crystal_count display
+        $("#crystal_count").html(crystal_count);
+      }
+      
+
+      //The player will gain a win if the crystal_count equals the crystal_target and they will lose if it goes over
+      //On either a win or loss the game will reset the crystal_target, the button values, and the crystal-count will be 0
+      if(crystal_count >= crystal_target){
+        //reset the buttons values
+        crystal_1 = new_crystal();  
+        crystal_2 = new_crystal();
+        crystal_3 = new_crystal();
+        crystal_4 = new_crystal();
+        //if the plyer wins
+        if(crystal_count == crystal_target){
+          wins++;
+          $("#wins").html("Wins: " + wins);
+          crystal_target = new_target();
+          $("#crystal_target").html(crystal_target);
+          crystal_count = 0;
+          $("#crystal_count").html(crystal_count);
+        }
+        //if the player loses
+        else if(crystal_count > crystal_target){
+          losses++;
+          $("#losses").html("Losses: " + losses);
+          crystal_target = new_target();
+          $("#crystal_target").html(crystal_target);
+          crystal_count = 0;
+          $("#crystal_count").html(crystal_count);
+        }
+      }
     }
 
 
